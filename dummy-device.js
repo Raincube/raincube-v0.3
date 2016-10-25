2bbe0c0ad86c27f2c9c99092223a98bf7c20b598
racifuina <racifuina@me.com>
2016-10-25 09:32:47 -0600

    env variables

--- dummy-device.js
+++ dummy-device.js
@@ -1,7 +1,13 @@
 var net = require('net');
-var socket = net.connect(3150);
-var id = "001"
-var enviarPesoCore = true;
+var id = process.env.DEVICE_ID || "001"
+var local = process.env.LOCAL || true
+var socket;
+
+if (local === true) {
+     socket = net.connect(3150);
+} else {
+    socket = net.connect(3150, "raincube-garden.us-west-2.elasticbeanstalk.com");
+}
 
 socket.on("connect", function (data) {
     console.log("Client: -> " + "id=" + id + "&r=0& (connect)");
